6b96dfc453bc91c6315b6e3c75cec722b24afb40
VivekDevShah <vivek65.ugcs20@iiitranchi.ac.in>
2023-04-12 03:35:19 +0530

    release first version

--- app.js
+++ app.js
@@ -2,24 +2,60 @@ const express=require("express");
 const ejs=require("ejs");
 const app=express();
 var $ = require('jquery')
+const bodyParser=require("body-parser");
 
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
 
+// home
 app.get("/", function(req, res){
     res.render("home", {title:"HOME"});
 });
 
-app.get("/login", function(req, res){
+// login
+app.route("/login")
+.get(function(req, res){
     res.render("login", {title: "Login Page"});
+})
+.post(function(req, res){
+    var userName=req.body.username;
+    var password=req.body.password;
+    if(userName==="admin" && password==="12345"){
+        res.redirect("/");
+    }
+    else{
+        res.render("incorrect-credentials", {title:"incorrect-credentials" });
+    }
 });
 
-app.get("/services", function(req, res){
-    res.render("services", {title: "Service Information"});
+// services
+app.route("/add-patient")
+.get(function(req, res){
+    res.render("add-patient", {title: "Add a Patient"});
+})
+.post(function(req, res){
+    res.console("hey");
 });
 
-app.get("/about", function(req, res){
-    res.render("about", {title: "About Us"});
+app.route("/get-patient")
+.get(function(req, res){
+    res.render("get-patient", {title: "Patient Details"});
+})
+.post(function(req, res){
+
+});
+
+app.route("/doctors-availability")
+.get(function(req, res){
+    res.render("doctors-availability", {title: "Check Availability of Doctors"});
+})
+.post(function(req, res){
+
+});
+
+app.get("/documentation", function(req, res){
+    res.render("documentation", {title: "Documentation"});
 });
 
 app.get("/contact", function(req, res){
